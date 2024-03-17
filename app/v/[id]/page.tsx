d785bddd267e47bb0491c2da7bdcd2ce4650b3a4
pushborem <161558076+pushborem@users.noreply.github.com>
2024-03-17 20:02:57 +0700

    Update page.tsx

--- app/v/[id]/page.tsx
+++ app/v/[id]/page.tsx
@@ -138,7 +138,10 @@ export default async function Video({ params }: PageProps) {
                             </TableBody>
                         </Table>
                         <div className="grid grid-cols-2 gap-2 mt-8 md:grid-cols-3 lg:grid-cols-2 lg:ml-4 lg:my-4">
-                            <Link href={`https://${upstream}/d/${file.filecode}`} className="col-span-full md:col-auto lg:col-span-full">
+                            <Link
+                                href={`https://${upstream}/d/${file.filecode}`}
+                                className="col-span-full md:col-auto lg:col-span-full"
+                            >
                                 <Button className="w-full">
                                     <DownloadIcon className="size-4 me-1 mb-1"></DownloadIcon>
                                     Download
@@ -148,11 +151,13 @@ export default async function Video({ params }: PageProps) {
                                 <Share1Icon className="size-4 me-1 mb-0.5"></Share1Icon>
                                 Share
                             </CopyButton>
-                           <LikeButton className="lg:col-span-full" useButton={true} file={file} >
-                                Like
-                            </LikeButton>
+                            <LikeButton
+                                className="lg:col-span-full"
+                                useButton={true}
+                                file={file}
+                            />
                         </div>
-                        </div>
+                    </div>
                 </CardContent>
             </Card>
             <p>Video bokep indo terbaru viral {file.title} di {SITENAME} Video Bokep Indo Jepang Barat Terbaru bocil jilbab smp indonesia mama sma hijab abg colmek film tante twitter asia hot download live stw situs nonton link sd crot playbokep simontok bokepin montok baru perawan anak kecil telegram selingkuh ojol cantik gay vidio lokal artis pelajar janda streaming jepang barat korea japan jav cina japanese china rusia arab india thailand hd anime hentai bokepind gudang avtub pijat sotwe rumah pemerkosaan inggris xpanas pure tobrut vcs ngintip binor remaja yandex update perselingkuhan wiki raja full com porno indoh</p>
